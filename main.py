a6f1029d480edbd288397eefd778cda1d5b57c67
Michail Lukyanov <lukyanov5626@yandex.ru>
2025-04-05 22:31:51 +0300

    Просто изменил название переменной

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.masks import get_mask_account, get_mask_card_number
 
-number_card = get_mask_card_number(2568475325984528)
+number_card = get_mask_card_number("2568475325984528")
 number_account = get_mask_account(15478562456985215489)
 
 print("Номер карты: " + number_card)
