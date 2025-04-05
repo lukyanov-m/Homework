fac3336ab3a2067b9e7a839a08d3b6bfc5afc1e7
Michail Lukyanov <lukyanov5626@yandex.ru>
2025-04-05 19:31:32 +0300

    Созданы первые две функции маскировки номеров карты и счета

---
+++ main.py
@@ -0,0 +1,7 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+number_card = get_mask_card_number(2568475325984528)
+number_account = get_mask_account(15478562456985215489)
+
+print("Номер карты: " + number_card)
+print("Номер счета: " + number_account)
---
+++ src/masks.py
@@ -0,0 +1,16 @@
+def get_mask_card_number(card_number: int) -> str:
+    """Функция разбивает номер карты по блокам и скрывает часть этого номера"""
+    str_card_number = str(card_number)
+    if len(str_card_number) != 16:
+        return "Не корректный номер карты"
+    block1 = str_card_number[:4]
+    block2 = str_card_number[4:6] + "**"
+    block3 = "****"
+    block4 = str_card_number[12:]
+    return f"{block1} {block2} {block3} {block4}"
+
+
+def get_mask_account(account_number: int) -> str:
+    """Функция возвращает последние четыре цифры номера счета"""
+    str_account_number = str(account_number)
+    return "**" + str_account_number[-4:]
