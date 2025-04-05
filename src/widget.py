d4cf5d4b4aeed0ebfc49e6c2c3c246dbbe82eeb0
Michail Lukyanov <lukyanov5626@yandex.ru>
2025-04-05 22:20:00 +0300

    В модуль widget добавлена функция get_date которая форматирует дату

--- src/widget.py
+++ src/widget.py
@@ -8,4 +8,8 @@ def mask_account_card(card_or_account: str) -> str:
     else:
         return f"{card_or_account[:-16]}{get_mask_card_number(card_or_account[-16:])}"
 
-print(mask_account_card("Счет 35383033474447895560"))
+
+def get_date(date_and_time: str) -> str:
+    """Принимает строку с датой и временем ГГГГ-ММ-ДД+время, возвращает дату в формате ДД.ММ.ГГГГ"""
+    list_date = date_and_time[:10].split("-")
+    return f"{list_date[2]}.{list_date[1]}.{list_date[0]}"
