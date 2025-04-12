39c67fb4349e22f59352f55963f8193d74caa5ca
Michail Lukyanov <lukyanov5626@yandex.ru>
2025-04-12 19:31:22 +0300

    В processing.py добавлена функция сортировки по дате. В функции filter_by_state изменил название переменной.

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,13 @@
-def filter_by_state(list_dict: list, state: str="EXECUTED") -> list:
-    """Функция фильтрует список задач по статусу их выполнения"""
-
-    new_list_for_state = []
+def filter_by_state(list_dict: list, state: str = "EXECUTED") -> list:
+    """Функция фильтрует список словарей с процессами по состоянию их выполнения"""
+    list_by_status = []
     for one_dict in list_dict:
         if one_dict["state"] == state:
-            new_list_for_state.append(one_dict)
-    return new_list_for_state
+            list_by_status.append(one_dict)
+    return list_by_status
+
+
+def sort_by_date(list_dict: list, rev_sort: bool = True) -> list:
+    """Функция сортирует список словарей с процессами по времени"""
+    list_by_date = sorted(list_dict, key=lambda one_dict: one_dict["date"], reverse=rev_sort)
+    return list_by_date
