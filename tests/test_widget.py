31d1fe0d5b2e49fccc0db9e70ad0523cf20b6c8a
Michail Lukyanov <lukyanov5626@yandex.ru>
2025-04-20 01:09:45 +0300

    widget. Тесты готовы

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.widget import mask_account_card
+from src.widget import mask_account_card, get_date
 
 
 @pytest.mark.parametrize(
@@ -26,3 +26,7 @@ def test_mask_account_card_with_invalid_type(invalid_types):
     for invalid_type in invalid_types:
         with pytest.raises(TypeError):
             mask_account_card(invalid_type)
+
+
+def test_get_date():
+    assert get_date("2024-03-11T02:26:18.671407") == "11.03.2024"
