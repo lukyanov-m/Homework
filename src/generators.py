9c55533ac5cb863c12d406fa4a73f57963a40771
Michail Lukyanov <lukyanov5626@yandex.ru>
2025-04-27 01:03:03 +0300

    В generators.py создана вторая функция transaction_descriptions

--- src/generators.py
+++ src/generators.py
@@ -1,10 +1,19 @@
-from typing import Iterator
+from typing import Dict, Iterator
 
 
-def filter_by_currency(list_dict: list, currency: str) -> Iterator[dict]:
+def filter_by_currency(list_dict: list[Dict], currency: str) -> Iterator[dict]:
     """
-    Функция генератор принимает список словарей и необходимую валюту, возвращает только словари с указанной валютой
+    Функция генератор принимает список словарей с транзакциями и необходимую валюту,
+    возвращает только словари с указанной валютой.
     """
     for operation in list_dict:
         if operation.get("operationAmount", {}).get("currency", {}).get("code") == currency:
             yield operation
+
+
+def transaction_descriptions(list_dict: list) -> Iterator[str]:
+    """
+    Генератор принимает список словарей с транзакциями, возвращает только описание транзакции.
+    """
+    for operation in list_dict:
+        yield operation.get("description")
