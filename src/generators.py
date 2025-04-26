0ef0b0c88d4fbaedb2edeb40387be3d05bbc33fa
Michail Lukyanov <lukyanov5626@yandex.ru>
2025-04-27 00:10:31 +0300

    Создан модуль generators.py, а в нем первая функция filter_by_currency

--- src/generators.py
+++ src/generators.py
@@ -6,5 +6,5 @@ def filter_by_currency(list_dict: list, currency: str) -> Iterator[dict]:
     Функция генератор принимает список словарей и необходимую валюту, возвращает только словари с указанной валютой
     """
     for operation in list_dict:
-        if operation["operationAmount"]["currency"]["code"] == currency:
+        if operation.get("operationAmount", {}).get("currency", {}).get("code") == currency:
             yield operation
