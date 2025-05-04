755c28b5a47597ea07c40fd1697da7181ec1ca14
Michail Lukyanov <lukyanov5626@yandex.ru>
2025-05-04 19:31:32 +0300

    Создан декоратор log, добавлен в рэдми.

--- src/decorators.py
+++ src/decorators.py
@@ -3,6 +3,7 @@ def log(filename=None):
     Декоратор записывает в указанный filename файл результат выполнения функции, в случае ошибки запишет эту ошибку
     и принятые функцией параметры. Если filename не указан, выведет информацию в консоль.
     """
+
     def decorator(func):
         def wrapper(*args, **kwargs):
             try:
@@ -11,18 +12,20 @@ def log(filename=None):
             except Exception as e:
                 message = f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}. \n"
                 if filename:
-                    with open(filename, 'a') as f:
+                    with open(filename, "a") as f:
                         f.write(message)
                 else:
                     print(message)
                 raise
 
             if filename:
-                with open(filename, 'a') as f:
+                with open(filename, "a") as f:
                     f.write(message)
             else:
                 print(message)
 
             return result
+
         return wrapper
+
     return decorator
