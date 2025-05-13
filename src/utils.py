990c320c956b1b73f132d281531755e081562ac5
Michail Lukyanov <lukyanov5626@yandex.ru>
2025-05-13 23:18:55 +0300

    Добавлена первая функция в utils.py

---
+++ src/utils.py
@@ -0,0 +1,22 @@
+import json
+from json import JSONDecodeError
+from typing import Any, Dict, List
+
+
+def get_financial_transaction_data(path: str) -> List[Dict[str, Any]]:
+    """Функция принимает путь до json файла, возвращает список словарей с данными о финансовых транзакциях"""
+    try:
+        with open(path, "r", encoding="utf-8") as bank_fail:
+            try:
+                bank_data = json.load(bank_fail)
+            except JSONDecodeError:
+                print("Ошибка декодирования файла")
+                return []
+    except FileNotFoundError:
+        print("Файл не найден")
+        return []
+
+    return bank_data
+
+
+
