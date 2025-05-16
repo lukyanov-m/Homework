57abdcac7018ba641a066bb35bd40631d110e16c
Michail Lukyanov <lukyanov5626@yandex.ru>
2025-05-17 01:34:56 +0300

    Написаны все функции по дз

---
+++ src/external_api.py
@@ -0,0 +1,29 @@
+import json
+import os
+import requests
+from dotenv import load_dotenv
+from typing import Any, Dict, List
+
+load_dotenv("C:/Users/Mishanya/PycharmProjects/Bank/.env")
+
+ERD_API_KEY = os.getenv("ERD_API_KEY")
+
+def get_currency_conversion(currency_to: str, currency_from: str, amount: float) -> List[Dict[str, Any]]:
+    """Функция конвертации валюты"""
+
+    url = f"https://api.apilayer.com/exchangerates_data/convert?to={currency_to}&from={currency_from}&amount={amount}"
+
+    headers = {
+      "apikey": ERD_API_KEY
+    }
+
+    response = requests.request("GET", url, headers=headers)
+
+    status_code = response.status_code
+    result = response.json()
+
+
+    if status_code != 200:
+        raise ValueError(f"Не удалось получить курс валюты")
+    else:
+        return result
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,8 @@ import json
 from json import JSONDecodeError
 from typing import Any, Dict, List
 
+from src.external_api import get_currency_conversion
+
 
 def get_financial_transaction_data(path: str) -> List[Dict[str, Any]]:
     """Функция принимает путь до json файла, возвращает список словарей с данными о финансовых транзакциях"""
@@ -19,4 +21,22 @@ def get_financial_transaction_data(path: str) -> List[Dict[str, Any]]:
     return bank_data
 
 
-print(get_financial_transaction_data("../data/operations.json"))
+def get_transaction_amount(transaction: int) -> float:
+    """функция, принимает на вход транзакцию и возвращает сумму транзакции в рублях.
+    Если транзакция была в USD или EUR, происходит обращение к внешнему API для получения
+    текущего курса валют и конвертации суммы операции в рубли"""
+
+    list_data = get_financial_transaction_data("../data/operations.json")
+
+    for data in list_data:
+        if data.get("id") != transaction:
+            continue
+        if data["operationAmount"]["currency"]["code"] == "RUB":
+            return data["operationAmount"]["amount"]
+        else:
+            return get_currency_conversion(
+                "RUB",
+                data["operationAmount"]["currency"]["code"],
+                data["operationAmount"]["amount"]
+            )["result"]
+
